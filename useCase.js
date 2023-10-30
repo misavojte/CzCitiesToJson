b957ff7e6a70d879835a5e09e88f67012f2ae025
misavojte <mich.vojtechovska@gmail.com>
2023-10-30 10:09:57 +0100

    improvement

---
+++ useCase.js
@@ -0,0 +1,33 @@
+/**
+ * Simplify Czech text for search. This function removes all diacritics and spaces.
+ * Must be used for searching in Czech municipalities.
+ * @param text {string} - Czech text.
+ * @returns {string} - Simplified text.
+ * @example simplifyCzechTextForSearch('Český Těšín') // 'ceskytesin'
+ */
+const simplifyCzechTextForSearch = text => {
+    text = text.replace(/ /g, "");
+    text = text.replace(/-/g, "");
+    text = text.replace(/–/g, "");
+    const czechToBasic = {
+        'á': 'a', 'č': 'c', 'ď': 'd', 'é': 'e', 'ě': 'e',
+        'í': 'i', 'ň': 'n', 'ó': 'o', 'ř': 'r', 'š': 's',
+        'ť': 't', 'ú': 'u', 'ů': 'u', 'ý': 'y', 'ž': 'z'
+    };
+    return Array.from(text.toLowerCase())
+               .map(char => czechToBasic[char] || char)
+               .join('');
+};
+
+/**
+ * Returns array of municipalities that match the search query
+ * @param query {string} - Search query - municipality name which went through simplifyCzechTextForSearch() function
+ * @param municipalityJson {{id: number, label: string, simpleName: string}[]} - Array of municipalities, provided in output.json
+ * @returns {{value: number, label: string}[]} - Array of municipalities that match the search query, where value is the id of the municipality
+ */
+
+const searchInMunicipalities = (query, municipalityJson) => {
+    return municipalityJson
+        .filter(municipality => municipality.simpleName.includes(query))
+        .map(municipality => ({value: municipality.id, label: municipality.label}));
+}
